6667ff36252838fc235d8073e607feebe46c7ba3
nikkojustix <nickolaykotov@gmail.com>
2022-07-26 16:46:09 +0300

    added slider logo

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 import * as flsFunctions from './modules/functions.js';
+import Swiper from 'swiper/bundle';
 
 flsFunctions.isWebp();
+
+console.log('1');
+const logoSwiper = new Swiper('.header__logo', {
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+});
