4019fc723963de72ccc0c5ce704c3f39c3ecd0fa
nikkojustix <nickolaykotov@gmail.com>
2022-07-27 16:44:02 +0300

    added products sections; bug with tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 import * as flsFunctions from './modules/functions.js';
 import Swiper from 'swiper/bundle';
+import { Navigation } from 'swiper';
 
 flsFunctions.isWebp();
 
-console.log('1');
 const logoSwiper = new Swiper('.header__logo', {
   speed: 500,
   autoplay: {
@@ -16,3 +16,179 @@ const logoSwiper = new Swiper('.header__logo', {
     crossFade: true,
   },
 });
+
+const menuDropdown = document.querySelector('.menu__link--dropdown');
+const menuSubmenu = document.querySelector('.menu__submenu');
+
+document.addEventListener('click', (e) => {
+  if (e.target !== menuDropdown) {
+    menuSubmenu.classList.remove('menu__submenu--opened');
+  } else {
+    e.preventDefault();
+    menuSubmenu.classList.toggle('menu__submenu--opened');
+  }
+});
+document.addEventListener('keyup', (e) => {
+  if (e.keyCode === 27) {
+    menuSubmenu.classList.remove('menu__submenu--opened');
+  }
+});
+
+const headerSwiper = new Swiper('.swiper-header', {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next--header',
+    prevEl: '.swiper-button-prev--header',
+  },
+});
+
+const cardsSwiper800 = new Swiper('.swiper-cards--800', {
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--800',
+    prevEl: '.swiper-button-prev--800',
+  },
+  pagination: {
+    el: '.swiper-pagination--800',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+});
+
+const cardsSwiper1000 = new Swiper('.swiper-cards--1000', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--1000',
+    prevEl: '.swiper-button-prev--1000',
+  },
+  pagination: {
+    el: '.swiper-pagination--1000',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiper1500 = new Swiper('.swiper-cards--1500', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--1500',
+    prevEl: '.swiper-button-prev--1500',
+  },
+  pagination: {
+    el: '.swiper-pagination--1500',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiper3000 = new Swiper('.swiper-cards--3000', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--3000',
+    prevEl: '.swiper-button-prev--3000',
+  },
+  pagination: {
+    el: '.swiper-pagination--3000',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiper4000 = new Swiper('.swiper-cards--4000', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--4000',
+    prevEl: '.swiper-button-prev--4000',
+  },
+  pagination: {
+    el: '.swiper-pagination--4000',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiperReusable = new Swiper('.swiper-cards--reusable', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--reusable',
+    prevEl: '.swiper-button-prev--reusable',
+  },
+  pagination: {
+    el: '.swiper-pagination--reusable',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiperCartriges = new Swiper('.swiper-cards--cartiges', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--cartiges',
+    prevEl: '.swiper-button-prev--cartiges',
+  },
+  pagination: {
+    el: '.swiper-pagination--cartiges',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiper12salt = new Swiper('.swiper-cards--12salt', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--12salt',
+    prevEl: '.swiper-button-prev--12salt',
+  },
+  pagination: {
+    el: '.swiper-pagination--12salt',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+const cardsSwiper20salt = new Swiper('.swiper-cards--20salt', {
+  loop: true,
+  speed: 500,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next--20salt',
+    prevEl: '.swiper-button-prev--20salt',
+  },
+  pagination: {
+    el: '.swiper-pagination--20salt',
+    clickable: true,
+  },
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+});
+
+flsFunctions.tabs('.tabs__nav-btn--one-off', '.tabs__content-item', 'tabs__nav-btn--active');
+flsFunctions.tabs('.tabs__nav-btn--liquids', '.tabs__content-item', 'tabs__nav-btn--active');
+
+// cardsSwiper800.updateSlides();
+flsFunctions.filter(cardsSwiper800);
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -4,8 +4,7 @@ export function isWebp() {
     webP.onload = webP.onerror = function () {
       callback(webP.height == 2);
     };
-    webP.src =
-      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
   }
 
   testWebP(function (support) {
@@ -16,5 +15,61 @@ export function isWebp() {
     // } else {
     //   document.querySelector('body').classList.add('no-webp');
     // }
+  });
+}
+
+export function tabs(tabSelector, contentSelector, activeClass) {
+  const tabs = document.querySelectorAll(tabSelector);
+  const contents = document.querySelectorAll(contentSelector);
+
+  function showContent(i = 0) {
+    tabs[i].classList.add(activeClass);
+    contents[i].style.display = 'block';
+  }
+  function hideContent() {
+    contents.forEach((content) => {
+      content.style.display = 'none';
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
+    });
+  }
+
+  hideContent();
+  showContent();
+
+  tabs.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+      hideContent();
+      showContent(i);
+    });
+  });
+}
+
+export function filter(swiper) {
+  const list = document.querySelectorAll('.filter__btn');
+  const items = document.querySelectorAll('.filter~* .card');
+  list.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const target = e.target.dataset.id;
+      list.forEach((item) => {
+        item.classList.remove('filter__btn--active');
+      });
+      item.classList.add('filter__btn--active');
+      if (target === 'all') {
+        items.forEach((item) => {
+          item.parentNode.style.display = 'block';
+        });
+      } else {
+        items.forEach((item) => {
+          if (item.dataset.id === target) {
+            item.parentNode.style.display = 'block';
+          } else {
+            item.parentNode.style.display = 'none';
+          }
+        });
+      }
+      swiper.update();
+    });
   });
 }
