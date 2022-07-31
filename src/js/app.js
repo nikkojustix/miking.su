b54c8dee76054a8b059221d65376e2db134a2b1e
nikkojustix <nickolaykotov@gmail.com>
2022-08-01 00:28:41 +0300

    добавлен скролл наверх

--- src/js/app.js
+++ src/js/app.js
@@ -251,3 +251,21 @@ function createFilterNode(index, ids) {
   }
   return ul;
 }
+
+// scroll to top
+const scrollBtn = document.querySelector('.scroll-to-top');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 200) {
+    scrollBtn.style.visibility = 'visible';
+    scrollBtn.style.opacity = '1';
+  } else {
+    scrollBtn.style.visibility = 'hidden';
+    scrollBtn.style.opacity = '0';
+  }
+});
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
